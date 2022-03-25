3c4b876f491d54fee89f06e55d5f6ae331a7c596
lorenaop <lorena_oliveirapires@hotmail.com>
2022-03-24 23:57:05 -0300

    commit inicial

---
+++ agenciareact/src/views/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function Agencia() {
+    return(
+        <main className="pagina-inicial">
+        <h2 className="bemvindo">BEM VINDO(A)!</h2><br />
+        <p>MELHORES PRAIAS DO PAIS COM O MELHOR PREÇO!</p>
+        <div style={{   
+        backgroundImage: `url("https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/fervedouro-tocantins-brazil.webp?w=1450&h=816")`,
+        height: '800px',
+        }}>
+        </div>
+    </main>
+
+        );
+}
